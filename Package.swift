52f66bf34748e6ca1c7beb1046b403720c5bbfcb
Georg Tuparev <gtupar@tuparev.com>
2020-05-17 03:54:43 +0300

    Refactoring the SPM

--- Package.swift
+++ Package.swift
@@ -7,9 +7,7 @@ let package = Package(
     name: "SoftwareEtudes",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "SoftwareEtudes",
-            targets: ["SoftwareEtudes"]),
+        .library(name: "SoftwareEtudes", targets: ["SoftwareEtudes"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +16,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "SoftwareEtudes",
-            dependencies: []),
-        .testTarget(
-            name: "SoftwareEtudesTests",
-            dependencies: ["SoftwareEtudes"]),
+        .target(name: "SoftwareEtudes", dependencies: []),
+        .testTarget(name: "SoftwareEtudesTests", dependencies: ["SoftwareEtudes"]),
     ]
 )
+
