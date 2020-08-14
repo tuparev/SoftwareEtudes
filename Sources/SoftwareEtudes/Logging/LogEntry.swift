d7054b7637edc4e48a2d3b3b8a706bc2fdada5b3
Georg Tuparev <gtupar@tuparev.com>
2020-08-14 18:05:19 +0300

    Documentation

--- Sources/SoftwareEtudes/Logging/LogEntry.swift
+++ Sources/SoftwareEtudes/Logging/LogEntry.swift
@@ -9,14 +9,67 @@
 
 import Foundation
 
-/// The levels are the same as `SwiftLog` levels.
-public enum LogLevel {
+/// Software Etudes' Logging is different than SwiftLog!
+///
+/// How?
+/// - It is simpler. No excessive use of Swift modernism. Our goal is to have simple and readable logging system that just works.
+/// - It allows complex client-server implementations. If needed, Etude's logging system could be used in high-performance, complex
+/// systems allowing near-realtime non-intrusive logging.
+/// - It could be use as trace-logger to measure time-critical processes.
+/// - It supports persistency.
+/// - If required, user's privacy could be protected.
+/// - Log messages are localisable (yes, English is not the only language!).
+/// - Handling log entries could be handled after time-critical processes/threads are executed.
+/// - Whenever possible, it integrates with other logging systems.
+/// - Could be integrated as part of domain specific logging systems.
+/// - It is fun to use!
+///
+///
+/// Software Etudes' Logging is similar with SwiftLog!
+///
+/// How?
+/// - Uses the same log levels
+/// - Whenever possible, uses same method names and equivalent types, so it makes it easy to be adopted with minimal refactoring.
+
+
+/// The Log Level
+///
+/// Log levels are ordered by their severity, with `.trace` being the least severe and `.critical` being the most severe.
+/// They are also `Int`s to allow to compare them.
+///
+/// It is recommended, that loggers will persist LogEntries with level `notice` of higher at least for some time.
+///
+/// The levels are the same as `SwiftLog` levels. But the LogLevel enum type is different!
+///
+public enum LogLevel: Int, Codable, CaseIterable {
+
+    /// Use this for tracing (time or execution  flow) of the process. Note that LogEntries could be made available
+    /// asynchronously at later stage. Use proper (visualisation) tools to create trace graph.
     case trace
+
+    /// Appropriate for messages that contain information to facilitate debugging of a program, often equivalent to
+    /// print() function calls.
     case debug
+
+    /// Appropriate for informational messages, often revealing the content of objects or structures.
     case info
+
+    /// Nothing to wary too much but worth noticing, specially in early or experimental stages. It is recommended to
+    /// make `notice`s and higher level LogEntries persistent.
     case notice
+
+    /// Appropriate for messages that are not error conditions yet, but more severe than `.notice`.
     case warning
+
+    /// Something really went wrong, but execution might continue (with caution).
     case error
+
+    /// Boom! A critical error conditions was encounter. After handling the LogEntry, it is recommended to terminate
+    /// further program execution.
+    ///
+    /// When a `critical` message is logged, the logging machinery should perform domain specific operations to capture
+    /// system state (such as capturing stack traces) to facilitate debugging. The logger might also to terminate the
+    /// process.
     case critical
 }
 
@@ -38,7 +91,7 @@ public protocol Logable {
 
 /// `LogEntry` provides super simple default implementation of `Logable` protocol.
 public struct LogEntry: Logable {
-    public private(set) var level: LogLevel = .`default`
+    public private(set) var level: LogLevel = .notice
     public private(set) var message: String?
     public private(set) var code: Int?
     
