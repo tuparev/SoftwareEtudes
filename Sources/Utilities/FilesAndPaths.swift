c76dd7aa388e1498358b83f8284a92010b07b630
Georg Tuparev <gtupar@tuparev.com>
2024-02-21 06:23:44 +0200

    Adding FilesAndPaths extensions to String and URL

---
+++ Sources/Utilities/FilesAndPaths.swift
@@ -0,0 +1,36 @@
+//
+//  FilesAndPaths.swift
+//  
+//
+//  Created by Georg Tuparev on 04/02/2022.
+//  Copyright © See Framework's LICENSE file
+//
+
+import Foundation
+
+public extension URL {
+
+    /// Returns App's default document directory
+    ///
+    /// This method is known to work only on macOS and iOS. It should work on other Apple platforms, but this is mot tested.
+    func documentsDirectory() -> URL {
+        let paths              = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+
+        return documentsDirectory
+    }
+}
+
+
+public extension String {
+
+    /// If `self` is supposed to be a path to a folder, it makes sure the last character is slash "/"
+    ///
+    /// If a `file://` type URL pointing to a folder is initialised with a string not ending with slash,
+    /// some URLs returned by several ``URL`` methods for some strange reason has no
+    /// URL schema. This method guarantees, that pats always end wit a slash.
+    func normalisedFolderPath() -> String {
+        if self.hasSuffix("/") { return self }
+        else                   { return "\(self)/" }
+    }
+}
--- Sources/Utilities/String+EmailAddressChecker.swift
+++ Sources/Utilities/String+EmailAddressChecker.swift
@@ -8,7 +8,14 @@
 
 import Foundation
 
+// A simple `String` extension, checking the validity of an email address
+
 extension String {
+
+    /// Checks if `self` is a valid email address format
+    ///
+    /// **Note:** This method does not check if the email address exists or not. It only validates the format
+    /// of `self`.
     public func isValidEmailAddress() -> Bool {
         guard self.lengthOfBytes(using: .utf8) > 5 else { return false }
 
