096255cdd5a0b68e951ce8247ef97a6634b3f9a7
Karen <karen@KarenM.local>
2024-06-12 12:53:38 +0400

    Added logger test cases.

---
+++ Tests/Logger/LoggerTest.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Karen on 12.06.24.
+//
+
+import XCTest
+import SoftwareEtudesLogger
+
+final class LoggerTests: XCTestCase {
+   
+    func test_log() {}
+    func test_logger() {}
+    func test_logger_channel() {}
+    func test_logger_with_channel_interpreter() {}
+}
