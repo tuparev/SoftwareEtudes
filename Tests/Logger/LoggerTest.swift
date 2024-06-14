70628c6215171aee81db448d8ee94d11c41ca3d4
Karen <karen@KarenM.local>
2024-06-14 17:33:46 +0400

    Added test cases.

--- Tests/Logger/LoggerTest.swift
+++ Tests/Logger/LoggerTest.swift
@@ -11,17 +11,32 @@ import XCTest
 
 final class LoggerTests: XCTestCase {
    
+    let message     = Message(payload: Message.Payload.code(code: 1),
+                              arguments:      ["password" : "!123456"],
+                              actions:        ["email"    : "test@domain.com"],
+                              formattingInfo: ["color"    : "#FF0000", "fontSize" : "14"])
+    
     func test_log() {}
-    func test_logger() {}
-    func test_logger_channel() {}
+    
+    
+    func test_logger() {
+        let log = MessageLogger(logLevel: .error, message: message)
+        XCTAssertNotNil(log)
+    }
+    
+    
+    func test_logger_channel() {
+        
+        let environment = MessageChannelEnvironment()
+        let interpreter = AbstractMessageInterpreter.init(environment: MessageInterpretingEnvironment.init())
+        let channel     = AbstractMessageChannel.init(environment: environment, interpreter: interpreter)
+        
+        XCTAssertNotNil(channel)
+    }
     
     
     func test_logger_with_channel_interpreter() {
         
-        let message     = Message(payload: Message.Payload.code(code: 1),
-                                  arguments:      ["password" : "!123456"],
-                                  actions:        ["email"    : "test@domain.com"],
-                                  formattingInfo: ["color"    : "#FF0000", "fontSize" : "14"])
         let environment = MessageChannelEnvironment()
         let interpreter = AbstractMessageInterpreter.init(environment: MessageInterpretingEnvironment.init())
         let channel     = AbstractMessageChannel.init(environment: environment, interpreter: interpreter)
