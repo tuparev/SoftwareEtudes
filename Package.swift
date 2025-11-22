1fd8acd77db6b591d6c04181fa5bcf6ec2968a50
Ani Klekchyan <a.klekchyan@tuparev.com>
2025-11-22 23:53:23 +0400

    Updated minimum deployment target to iOS 16

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SoftwareEtudes",
     defaultLocalization: "en",
-    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v10), .visionOS(.v1)],
+    platforms: [.macOS(.v14), .iOS(.v16), .tvOS(.v17), .watchOS(.v10), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name: "SoftwareEtudesUtilities",               targets: ["SoftwareEtudesUtilities"]),
