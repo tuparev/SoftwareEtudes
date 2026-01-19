9499744289f236e3f7010155125407e62580061d
Georg Tuparev <gtupar@tuparev.com>
2026-01-19 07:46:05 +0200

    Fixing setAllowedArguments

--- Sources/ExecutableConfiguration/CommandLineParser.swift
+++ Sources/ExecutableConfiguration/CommandLineParser.swift
@@ -103,7 +103,7 @@ open class CommandLineParser: Configuring {
     }
 
     public func setAllowedArguments(_ arguments: [String]) throws {
-        for anArgument in arguments {
+        for anArgument in originalArguments {
             if !(anArgument.hasPrefix("--") || anArgument.hasPrefix("-")) { throw CommandLineParserError.invalidArgumentFormat }
             guard let index = arguments.firstIndex(of: anArgument) else   { throw CommandLineParserError.invalidArgumentFormat }
         }
