0a1e5f12cc7fae2b2ec63d614d4dd496fe770a2c
Georg Tuparev <gtupar@tuparev.com>
2026-02-09 19:17:38 +0200

    Reversed some changes from the base branch

--- Package.swift
+++ Package.swift
@@ -12,9 +12,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name:    "SoftwareEtudesUtilities",               targets: ["SoftwareEtudesUtilities"]),
         .library(name:    "SoftwareEtudesExecutableConfiguration", targets: ["SoftwareEtudesExecutableConfiguration"]),
-//        .library(name:    "SoftwareEtudesLogging",                 targets: ["SoftwareEtudesLogging"]),
-//        .library(name:    "SoftwareEtudesCoreMessageDispatching",  targets: ["SoftwareEtudesCoreMessageDispatching"]),
-//        .executable(name: "logging-sandbox",                       targets: ["LoggingSandbox"]),
+        .library(name:    "SoftwareEtudesLogging",                 targets: ["SoftwareEtudesLogging"]),
+        .library(name:    "SoftwareEtudesCoreMessageDispatching",  targets: ["SoftwareEtudesCoreMessageDispatching"]),
+        .executable(name: "logging-sandbox",                       targets: ["LoggingSandbox"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git",     from: "1.0.0"),
@@ -25,30 +25,30 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(name: "SoftwareEtudesUtilities",               dependencies: [], path: "Sources/Utilities"),
         .target(name: "SoftwareEtudesExecutableConfiguration", dependencies: [], path: "Sources/ExecutableConfiguration"),
-//        .target(name: "SoftwareEtudesLogging",
-//                dependencies: [.product(name: "Logging", package: "swift-log"), "SoftwareEtudesCoreMessageDispatching"],
-//                path: "Sources/MessageDispatching/Logging",
-//                resources: [
-//                    .process("Documentation.docc")
-//                ]),
-//        .target(name: "SoftwareEtudesCoreMessageDispatching",  dependencies: ["SoftwareEtudesUtilities"], path: "Sources/MessageDispatching/CoreMessageDispatching",
-//                resources: [
-//                    .copy("model.json"),
-//                    .copy("TestData"),
-//                    .process("Documentation.docc")
-//                ]),
+        .target(name: "SoftwareEtudesLogging",
+                dependencies: [.product(name: "Logging", package: "swift-log"), "SoftwareEtudesCoreMessageDispatching"],
+                path: "Sources/MessageDispatching/Logging",
+                resources: [
+                    .process("Documentation.docc")
+                ]),
+        .target(name: "SoftwareEtudesCoreMessageDispatching",  dependencies: ["SoftwareEtudesUtilities"], path: "Sources/MessageDispatching/CoreMessageDispatching",
+                resources: [
+                    .copy("model.json"),
+                    .copy("TestData"),
+                    .process("Documentation.docc")
+                ]),
 
         .testTarget(name: "SoftwareEtudesUtilitiesTests",               dependencies: ["SoftwareEtudesUtilities"],               path: "Tests/Utilities"),
         .testTarget(name: "SoftwareEtudesExecutableConfigurationTests", dependencies: ["SoftwareEtudesExecutableConfiguration"], path: "Tests/ExecutableConfiguration"),
-//        .testTarget(name: "SoftwareEtudesLoggingTests",                 dependencies: ["SoftwareEtudesLogging"],                 path: "Tests/MessageDispatching/Logging"),
-//        .testTarget(name: "SoftwareEtudesCoreMessageDispatchingTests",  dependencies: ["SoftwareEtudesCoreMessageDispatching"],  path: "Tests/MessageDispatching/CoreMessageDispatching"),
-//        .executableTarget(
-//            name: "LoggingSandbox",
-//            dependencies: [
-//                .target(name: "SoftwareEtudesLogging")
-//            ],
-//            path: "Sources/LoggingSandbox"
-//        ),
+        .testTarget(name: "SoftwareEtudesLoggingTests",                 dependencies: ["SoftwareEtudesLogging"],                 path: "Tests/MessageDispatching/Logging"),
+        .testTarget(name: "SoftwareEtudesCoreMessageDispatchingTests",  dependencies: ["SoftwareEtudesCoreMessageDispatching"],  path: "Tests/MessageDispatching/CoreMessageDispatching"),
+        .executableTarget(
+            name: "LoggingSandbox",
+            dependencies: [
+                .target(name: "SoftwareEtudesLogging")
+            ],
+            path: "Sources/LoggingSandbox"
+        ),
     ],
 //    swiftLanguageModes: [.v5]
 )
