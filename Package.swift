e0e66f13407907c889a1a4750f38dcae093ce82f
Georg Tuparev <gtupar@tuparev.com>
2026-03-30 06:52:27 +0300

    Changing OS verions

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "SoftwareEtudes",
     defaultLocalization: "en",
-//    platforms: [.macOS(.v14), .iOS(.v18), .tvOS(.v18), .watchOS(.v11), .visionOS(.v1)],
-    platforms: [.macOS(.v14), .iOS(.v17),],
+    platforms: [.macOS(.v14), .iOS(.v17), .tvOS(.v17), .watchOS(.v10), .visionOS(.v1)],
+//    platforms: [.macOS(.v14), .iOS(.v17),],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(name:    "SoftwareEtudesUtilities",               targets: ["SoftwareEtudesUtilities"]),
