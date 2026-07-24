f2593b2a553f4a3647c82d7e86dce664a24d23f7
Georg Tuparev <gtupar@tuparev.com>
2026-07-24 14:22:43 +0300

    Defining (preliminarily) all ConfigurationValueTypes

--- Sources/ExecutableConfiguration/ConfigurationProperty.swift
+++ Sources/ExecutableConfiguration/ConfigurationProperty.swift
@@ -12,11 +12,41 @@ public enum ConfigurationValueType : Sendable{
     case bool(Bool)
     case int(Int)
     case double(Double)
+    case timestamp(Date)
+    case data(Data)
+    case array([ConfigurationValueType])
+    case dictionary([String : ConfigurationValueType])
+
+    case unset
+    case null
+    case NaN
+    case infinity
 }
 
 public struct ConfigurationProperty: Sendable {
-    public let key: String
+    // Required properties
+    public let keyPath: String
     public var value: ConfigurationValueType
 
-    
+    // Optional properties
+    public var isRequired: Bool = false
 }
+
+
+/*
+ - aliases (optional)
+ - valueType (optional), if not set -> inferred
+ - defaultValue(Set) (optional)
+ - description/help (optional)
+ - allowedValueSet (optional)
+ - min/max value (optional)
+ - origin {domain : setting} hierarchy - computed dynamically.
+
+ Settings could be:
+ - simple values (e.g. timeout=30)
+ - dictionaries
+ - arrays
+ - key-path (will be flatten to one of the above possibilities), and exist because is very useful notation.
+
+ */
+
